86af7194ac3c265046bb0f0473f94551abc57565
masoodtalha <masoodtalha7@gmail.com>
2016-10-07 02:51:21 +0500

    fix(calendar): Style of rows

--- app/components/views/Calendar/index.js
+++ app/components/views/Calendar/index.js
@@ -24,50 +24,86 @@ function Calendar(props) {
   return (
     <div className="container-fluid">
       <div className={`row ${styles.hourSection}`}>
-        <div className="col-xs-1 container">
-          <div className="row">8:00 AM</div>
-          <div className="row">8:30 AM</div>
+
+        <div className={`row ${styles.hourHalfSection} `}> 
+          <div className={`col-xs-1`}>
+            8:00 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
-        <div className={`col-xs-11 ${styles.hourLine}`}>
-          .col-xs-11
+        <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}> 
+          <div className={`col-xs-1`}>
+            8:30 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
       </div>
+
+
       <div className={`row ${styles.hourSection}`}>
-      <div className="col-xs-1 container">
-        <div className="row">9:00 AM</div>
-        <div className="row">9:30 AM</div>
-      </div>
-        <div className={`col-xs-11 ${styles.hourLine}`}>
-          .col-xs-11
+
+        <div className={`row ${styles.hourHalfSection} `}> 
+          <div className={`col-xs-1`}>
+            9:00 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
+        </div>
+        <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}> 
+          <div className={`col-xs-1`}>
+            9:30 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
       </div>
+
       <div className={`row ${styles.hourSection}`}>
-        <div className="col-xs-1 container">
-          <div className="row">10:00 AM</div>
-          <div className="row">10:30 AM</div>
+
+        <div className={`row ${styles.hourHalfSection} `}> 
+          <div className={`col-xs-1`}>
+            10:00 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
-        <div className={`col-xs-11 ${styles.hourLine}`}>
-          .col-xs-11
+        <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}> 
+          <div className={`col-xs-1`}>
+            10:30 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
       </div>
+
       <div className={`row ${styles.hourSection}`}>
-        <div className="col-xs-1 container">
-          <div className="row">11:00 AM</div>
-          <div className="row">11:30 AM</div>
+
+        <div className={`row ${styles.hourHalfSection} `}> 
+          <div className={`col-xs-1`}>
+            11:00 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
-        <div className={`col-xs-11 ${styles.hourLine}`}>
-          .col-xs-11
+        <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}> 
+          <div className={`col-xs-1`}>
+            11:30 AM
+          </div>
+          <div className={`col-xs-11`}>
+            .col-xs-11
+          </div>
         </div>
       </div>
-      <div className={`row ${styles.hourSection}`}>
-        <div className="col-xs-1 container">
-          <div className="row">12:00 AM</div>
-          <div className="row">12:30 AM</div>
-        </div>
-        <div className={`col-xs-11 ${styles.hourLine}`}>
-          .col-xs-11
-        </div>
-      </div>
+
     </div>
   );
 }
