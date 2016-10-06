8047ba4263c7172c1ca42f9fab2b9c9503c0c6e1
Muhammad Nadeem Azam <mavburewala@gikian.com>
2016-10-07 03:30:15 +0500

    feat(calendar): converted calendar view to class

--- app/components/views/Calendar/index.js
+++ app/components/views/Calendar/index.js
@@ -19,61 +19,62 @@
 
  import styles from './styles.css';
 
-function Calendar(props) {
+ export class Calendar extends React.Component {
 
-  console.log(props.endTime);
-  let hours = [];
-  let start = 0;
-  let index = 0;
-  let timeOfTheDay = 'AM'
-  while(start <= props.endTime){
-    let hour = 0 ;
-    if(index > 4 && timeOfTheDay !== 'PM'){
-      timeOfTheDay = 'PM';
-      index = 1;
+   componentDidMount(){
+      console.log("I am loaded")
+   }
+   render() {
+     let hours = [];
+     let start = 0;
+     let index = 0;
+     let timeOfTheDay = 'AM'
+     while(start <= this.props.endTime){
+       let hour = 0 ;
+       if(index > 4 && timeOfTheDay !== 'PM'){
+         timeOfTheDay = 'PM';
+         index = 1;
 
-    }
-    if(timeOfTheDay === 'AM'){
-      hour = index + 8;
-    }
-    else{
-      hour = index;
-    }
-    index ++ ;
-    start = start + 60;
-    //if(hour === 12){ timeOfTheDay = 'PM'; }
-    hours.push({start: hour+ ':00 '+ timeOfTheDay, middle: hour+ ':30 '+ timeOfTheDay})
-    console.log(start);
-  }
-  console.log("hours: ", hours);
-  return (
-    <div>
-    { hours.map(function (hour, i) {
-      console.log("Hour: ", hour);
-      return <div className="container-fluid" key={i}>
-        <div className={`row ${styles.hourSection}`}>
-          <div className={`row ${styles.hourHalfSection} `}>
-            <div className={`col-xs-1`}>
-              {hour.start}
-            </div>
-            <div className={`col-xs-11`}>
-              .col-xs-11
-            </div>
-          </div>
-          <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}>
-            <div className={`col-xs-1`}>
-              {hour.middle}
-            </div>
-            <div className={`col-xs-11`}>
-              .col-xs-11
-            </div>
-          </div>
-        </div>
-        </div>;
-      })}
-    </div>
-  );
-}
+       }
+       if(timeOfTheDay === 'AM'){
+         hour = index + 8;
+       }
+       else{
+         hour = index;
+       }
+       index ++ ;
+       start = start + 60;
+       //if(hour === 12){ timeOfTheDay = 'PM'; }
+       hours.push({start: hour+ ':00 '+ timeOfTheDay, middle: hour+ ':30 '+ timeOfTheDay})
+     }
+     console.log("hours: ", hours);
+     return (
+       <div className="container-fluid" id = "dayView">
+       { hours.map(function (hour, i) {
+         return <div className={`row ${styles.hourSection}`} key={i}>
+             <div className={`row ${styles.hourHalfSection} `}>
+               <div className={`col-xs-1`}>
+                 {hour.start}
+               </div>
+               <div className={`col-xs-11`}>
+                 .col-xs-11
+               </div>
+             </div>
+             <div className={`row ${styles.hourHalfSection} ${styles.dottedBorderTop} ${styles.solidBorderBottom}`}>
+               <div className={`col-xs-1`}>
+                 {hour.middle}
+               </div>
+               <div className={`col-xs-11`}>
+                 .col-xs-11
+               </div>
+             </div>
+           </div>;
+         })}
+       </div>
+     );
+   }
+ }
+
 
 Calendar.propTypes = {
   endTime: PropTypes.number,
