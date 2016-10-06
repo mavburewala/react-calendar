e3dcf6f2f8843d2841996b5fdd6ad352cb4a0841
Muhammad Nadeem Azam <mavburewala@gikian.com>
2016-10-06 23:45:03 +0500

    chore(cleaning): splitted components

---
+++ app/components/views/Home/index.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * Home.react.js
+ *
+ * A common button, if you pass it a prop "route" it'll render a link to a react-router route
+ * otherwise it'll render a link with an onclick
+ */
+
+ import React from 'react';
+ import Helmet from 'react-helmet';
+
+ import messages from './messages';
+
+
+ import { FormattedMessage } from 'react-intl';
+ import Button from 'components/widgets/Button';
+ import H2 from 'components/widgets/H2';
+
+ import styles from './styles.css';
+
+function Home(props) {
+
+
+  return (
+    <article>
+      <Helmet
+        title="Home Page"
+        meta={[
+          { name: 'description', content: 'A React.js Calendar application homepage' },
+        ]}
+      />
+      <div>
+        <section className={`${styles.textSection} ${styles.centered}`}>
+          <H2>
+            <FormattedMessage {...messages.startHeader} />
+          </H2>
+          <p>
+            <FormattedMessage {...messages.startMessage} />
+          </p>
+        </section>
+        <section className={styles.textSection}>
+          <H2>
+            <FormattedMessage {...messages.trymeHeader} />
+          </H2>
+        </section>
+      </div>
+    </article>
+  );
+}
+
+Home.propTypes = {
+  
+};
+
+export default Home;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,28 +9,13 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import Helmet from 'react-helmet';
 
-import messages from './messages';
 import { createStructuredSelector } from 'reselect';
 
 
-import { FormattedMessage } from 'react-intl';
-import Button from 'components/widgets/Button';
-import H2 from 'components/widgets/H2';
-import List from 'components/widgets/List';
-import ListItem from 'components/widgets/ListItem';
-import LoadingIndicator from 'components/widgets/LoadingIndicator';
-
-import styles from './styles.css';
+import Home from 'components/views/Home';
 
 export class HomePage extends React.Component {
-  /**
-   * when initial state username is not null, submit the form to load repos
-   */
-  componentDidMount() {
-    if (this.props.username && this.props.username.trim().length > 0) {
-      this.props.onSubmitForm();
-    }
-  }
+
   /**
    * Changes the route
    *
@@ -44,29 +29,7 @@ export class HomePage extends React.Component {
   render() {
 
     return (
-      <article>
-        <Helmet
-          title="Home Page"
-          meta={[
-            { name: 'description', content: 'A React.js Calendar application homepage' },
-          ]}
-        />
-        <div>
-          <section className={`${styles.textSection} ${styles.centered}`}>
-            <H2>
-              <FormattedMessage {...messages.startHeader} />
-            </H2>
-            <p>
-              <FormattedMessage {...messages.startMessage} />
-            </p>
-          </section>
-          <section className={styles.textSection}>
-            <H2>
-              <FormattedMessage {...messages.trymeHeader} />
-            </H2>
-          </section>
-        </div>
-      </article>
+      <Home></Home>
     );
   }
 }
