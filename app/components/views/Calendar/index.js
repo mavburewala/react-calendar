371143f3570ed95db4956b661eb5b7caa8c24db9
Muhammad Nadeem Azam <mavburewala@gikian.com>
2016-10-08 05:58:42 +0500

    feat(calendar): conflicting events issue fixed

--- app/components/views/Calendar/index.js
+++ app/components/views/Calendar/index.js
@@ -12,6 +12,8 @@
 
  import messages from './messages';
 
+ import _ from 'underscore';
+
 
  import { FormattedMessage } from 'react-intl';
  import Button from 'components/widgets/Button';
@@ -39,6 +41,51 @@
      });
    };
 
+   isConflicting(eventA, eventB){
+     return (eventA.start+ eventA.duration) > eventB.start;
+   }
+
+   intersectingEvents(r1, r2) {
+      return !(r2.left > r1.right ||
+               r2.right < r1.left ||
+               r2.top > r1.bottom ||
+               r2.bottom < r1.top);
+    }
+
+   allConflictingEvents(event, events){
+
+     for(let i = 0; i < events.length; i++){
+       const eventA = {
+          left:   events[i].left,
+          top:    events[i].top,
+          right:  events[i].left+ 200,
+          bottom: events[i].top + events[i].height
+        };
+
+        const eventB = {
+          left:   event.left,
+          top:    event.top,
+          right:  event.left+ 200,
+          bottom: event.top + event.height
+        };
+       if(this.intersectingEvents(eventA, eventB)){
+         console.log("Intersecting: ", events[i], event, eventA, eventB);
+         event.left = events[i].left + 210;
+       }
+     }
+   }
+
+   calculateConflictingEvents(){
+     let events = this.props.events;
+     events = _.sortBy(events, 'start');
+     for(let i = 0; i< events.length; i++){
+       events[i].top = events[i].start * (2000/540);
+       events[i].height = events[i].duration * (2000/540),
+       events[i].left = 15;
+       this.allConflictingEvents(events[i], events.slice(0, _.indexOf(events, events[i])));
+     }
+     return events;
+   }
 
    render() {
      let hours = [];
@@ -63,7 +110,6 @@
        //if(hour === 12){ timeOfTheDay = 'PM'; }
        hours.push({start: hour+ ':00 '+ timeOfTheDay, middle: hour+ ':30 '+ timeOfTheDay})
      }
-     console.log("this.state: ", this.state);
      return (
        <div className={`container-fluid ${styles.dayViewMain}`} id = "dayView">
           <div className={`container-fluid ${styles.dayViewSkeleton}`}>
@@ -77,11 +123,14 @@
 
           </div>
           <div className={`col-xs-10 ${styles.eventsSection}`} id = {`eventsSection`}>
-            { this.props.events.map(function (event, i) {
+            { this.calculateConflictingEvents(this.props.events).map(function (event, i) {
               return <div
                 key={i}
-                className={`${styles.eventEntry}`}
-                style={{top: event.start * (2000/540), height: event.duration * (2000/540), width: 610}}
+                className={`row ${styles.eventEntry}`}
+                style={{top: event.start * (2000/540),
+                  height: event.duration * (2000/540),
+                  left: event.left? event.left: 15,
+                }}
                 >
                   {event.title}
                 </div>;
