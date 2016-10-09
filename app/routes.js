332e2651cdd70e623019583c7501fcaa3d6c61b2
Muhammad Nadeem Azam <mavburewala@gikian.com>
2016-10-10 01:14:03 +0500

    chore(linting): fixed routes linting issue

--- app/routes.js
+++ app/routes.js
@@ -23,13 +23,12 @@ export default function createRoutes(store) {
       getComponent(nextState, cb) {
         const importModules = Promise.all([
           System.import('containers/HomePage/reducer'),
-          System.import('containers/HomePage/sagas'),
           System.import('containers/HomePage'),
         ]);
 
         const renderRoute = loadModule(cb);
 
-        importModules.then(([reducer, sagas, component]) => {
+        importModules.then(([reducer, component]) => {
           injectReducer('home', reducer.default);
           renderRoute(component);
         });
