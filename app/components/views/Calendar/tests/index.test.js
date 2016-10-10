aabcfc47c449aecb58535bfbacbeeece048d237d
masoodtalha <masoodtalha7@gmail.com>
2016-10-10 13:10:58 +0500

    feat(test): added calendar tests

--- app/components/views/Calendar/tests/index.test.js
+++ app/components/views/Calendar/tests/index.test.js
@@ -1,1 +1,10 @@
+import expect from 'expect';
+import { shallow } from 'enzyme';
+import React from 'react';
 
+import Calendar from '../index';
+
+describe('<Calendar />', () => {
+  it('should render Calendar', () => {
+  });
+});
